ef79739ff1179552249ee787defff0c4aed0f48b
Tom Mason <wheybags@wheybags.com>
2011-12-02 20:36:49 +0000

    reads from stdin

--- pastebin.py
+++ pastebin.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
+
+# Usage: ./pastebin.py filename
+# use - as filename for stdin
+# Requires http://wwwsearch.sourceforge.net/mechanize/
+
 import mechanize
 import sys
+import os
 
 br = mechanize.Browser()
 br.open("http://pastebin.com/")
 
 br.select_form("myform")
 
-br["paste_code"] = str(open(sys.argv[1]).read())
+if sys.argv[1] == "-":
+	file = sys.stdin
+else:
+	file = open(sys.argv[1])
+	br["paste_name"] = os.path.basename(sys.argv[1])
+
+br["paste_code"] = str(file.read())
 
 print br.submit().geturl() 
